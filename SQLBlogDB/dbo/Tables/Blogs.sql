d6839918b8781ad20ed0883c553d9744e4b057c7
bhushan7717 <bhushan.wadile@yahoo.com>
2023-04-13 10:30:03 +0530

    Add project files.

---
+++ SQLBlogDB/dbo/Tables/Blogs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Blogs] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [Url]   NVARCHAR (MAX) NOT NULL,
+    [Title] NVARCHAR (200) DEFAULT (N'') NOT NULL,
+    CONSTRAINT [PK_Blogs] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ SQLBlogDB/dbo/Tables/Cars.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cars] (
+    [LicensePlate] NVARCHAR (450) NOT NULL,
+    [Make]         NVARCHAR (MAX) NOT NULL,
+    [Model]        NVARCHAR (MAX) NOT NULL,
+    [CreateDate]   DATETIME2 (7)  DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Cars] PRIMARY KEY CLUSTERED ([LicensePlate] ASC)
+);
+
